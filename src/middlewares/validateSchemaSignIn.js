36f5f443c85acd67854ef7910eb618a3471d72f0
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2022-07-08 19:27:44 -0300

    concertando o erro

--- src/middlewares/validateSchemaSignIn.js
+++ src/middlewares/validateSchemaSignIn.js
@@ -1,4 +1,4 @@
-import signInSchema from "../Schema/signInSchema";
+import signInSchema from "../Schema/signInSchema.js";
 
 export default async function validateSchemaSignIn(req,res,next) { 
     const validation = signInSchema.validate(req.body); 
