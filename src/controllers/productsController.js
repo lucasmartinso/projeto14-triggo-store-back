a5cdf4d85caf415c6d88f471fa9c6d2b07cb258e
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2022-07-08 15:25:56 -0300

    teste

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -2,11 +2,9 @@ import { db } from "../database/mongo.js";
 import dotenv from "dotenv";
 dotenv.config();
 
-const productList = process.env.PRODUCT_LIST;
-
 export async function getProducts(req, res) {
   try {
-    return res.send(productList);
+    return res.send("Oi");
   } catch (err) {
     return res.sendStatus(500);
   }
