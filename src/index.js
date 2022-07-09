65396bfd2ce1f30aeeb808c09b5f27888d37e6e9
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2022-07-09 11:05:10 -0300

    produtos

--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
 import express from "express";
 import chalk from "chalk";
 import cors from "cors";
-import { MongoClient, ObjectId } from "mongodb";
-import "dayjs/locale/pt-br.js";
-import dayjs from "dayjs";
 import dotenv from "dotenv";
-import joi from "joi";
-import bcrypt from "bcrypt";
-import { v4 as uuid } from "uuid";
 import userRouter from "./routes/userRouter.js";
+import productsRouter from "./routes/productsRouter.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 dotenv.config();
 
-app.use(userRouter);
+app.use(userRouter,productsRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(chalk.blue.bold(`\nRodando na porta ${process.env.PORT}`));
