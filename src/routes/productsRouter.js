eb978ee835e2475652823344ea0e12c121b8f6c3
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2022-07-11 21:04:35 -0300

    testando bonus

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -1,9 +1,7 @@
 import {
-  deleteItemBag,
   getBag,
   getProducts,
   postAdrress,
-  updateItemBag,
   postProducts,
   deleteProducts,
   sendProductsBag,
@@ -20,8 +18,6 @@ router.get("/products", getProducts);
 router.post("/main", validateUserMiddleware, getProducts);
 router.post("/bag", validateUserMiddleware, sendProductsBag);
 router.get("/buy", validateUserMiddleware, getBag);
-router.put("/buy", validateUserMiddleware, updateItemBag);
-router.delete("/buy", validateUserMiddleware, deleteItemBag);
 router.post("/finish", validateUserMiddleware, postAdrress);
 
 export default router;
